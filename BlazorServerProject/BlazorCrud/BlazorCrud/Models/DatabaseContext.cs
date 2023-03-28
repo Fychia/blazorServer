1346b167c1ca62b3c57ead304a4a61eeafccc54f
Rafael De Assis Valério <83471940+Fychia@users.noreply.github.com>
2023-03-28 18:01:29 -0300

    Project

---
+++ BlazorServerProject/BlazorCrud/BlazorCrud/Models/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        //Constructor
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        //Properties
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
---
+++ BlazorServerProject/BlazorCrud/BlazorCrud/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrud.Models
+{
+    public class Person
+    {
+        //Properties
+
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+
+        
+        
+        
+        
+        
+        
+        
+
+    }
+}
